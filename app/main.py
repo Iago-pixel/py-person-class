b1cefc719cac4df8ee48b7f203ec6333666b5d83
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-04-22 05:31:19 -0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -10,12 +10,10 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
 
-
     for index, person in enumerate(people):
         if person.get("wife"):
             person_list[index].wife = Person.people[person["wife"]]
         elif person.get("husband"):
             person_list[index].husband = Person.people[person["husband"]]
-
 
     return person_list
